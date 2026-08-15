cab542457b81266129f65092c157784abb90ed4e
Agustin Ruben Fernandez <98957122+Endolft@users.noreply.github.com>
2026-08-15 13:44:01 +0000

    Make idempotent create VPC

--- infra/vpc/create_vpc.sh
+++ infra/vpc/create_vpc.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
+
+set -e
+
 export AWS_ACCESS_KEY_ID="test"
 export AWS_SECRET_ACCESS_KEY="test"
 export AWS_DEFAULT_REGION="us-east-1"
 
 REGION="us-east-1"
 ENDPOINT="http://localhost:4566"
+VPC_NAME="taller-vpc"
+
+
+VPC_ID=$(aws ec2 describe-vpcs \
+  --filters "Name=tag:Name,Values=$VPC_NAME" \
+  --region $REGION \
+  --endpoint-url $ENDPOINT \
+  --query 'Vpcs[0].VpcId' \
+  --output text)
+
+if [ "$VPC_ID" != "None" ] && [ -n "$VPC_ID" ]; then
+    echo "La VPC '$VPC_NAME' ya existe con ID: $VPC_ID."
 
 echo "Creando la VPC..."
 VPC_ID=$(aws ec2 create-vpc \
@@ -13,6 +28,12 @@ VPC_ID=$(aws ec2 create-vpc \
   --endpoint-url $ENDPOINT \
   --query 'Vpc.VpcId' \
   --output text)
+
+  aws ec2 create-tags \
+      --resources $VPC_ID \
+      --tags Key=Name,Value=$VPC_NAME \
+      --region $REGION \
+      --endpoint-url $ENDPOINT
 
 if [ -z "$VPC_ID" ]; then
     echo "❌ ERROR: No se pudo obtener el ID de la VPC."
